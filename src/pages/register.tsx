d2143d2f370b0a59453d55b4f61f07d6e8df6ae5
Eleonora Nocentini Sköldebrink <eleonora.nocentini@gmail.com>
2023-11-19 17:59:30 +0100

    signin.tsx fixed

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Link from "next/link";
 
 export default function Register() {
   const [formData, setFormData] = useState({
@@ -7,16 +8,15 @@ export default function Register() {
     password: "",
   });
   const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
+  const [isRegistered, setIsRegistered] = useState(false);
 
   const handleChange = (e: any) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = async (e: any) => {
+  const handleRegister = async (e: any) => {
     e.preventDefault();
     setError("");
-    setSuccess("");
 
     const API_URL = "http://127.0.0.1:3013/api/v1/user";
 
@@ -32,14 +32,9 @@ export default function Register() {
       const result = await response.json();
 
       if (!response.ok) {
-        const errorMessage = result.data || "Ett fel uppstod vid registrering.";
-        if (errorMessage.includes("user already exists")) {
-          setError("Användaren finns redan.");
-        } else {
-          setError(errorMessage);
-        }
+        setError(result.data || "Ett fel uppstod vid registrering.");
       } else {
-        setSuccess("Registrering lyckades! Vänligen logga in.");
+        setIsRegistered(true);
       }
     } catch (error) {
       setError("Ett oväntat fel inträffade.");
@@ -48,36 +43,45 @@ export default function Register() {
 
   return (
     <div>
-      <h1>Registrera</h1>
-      {error && <p className="error">{error}</p>}
-      {success && <p className="success">{success}</p>}
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="username"
-          value={formData.username}
-          onChange={handleChange}
-          placeholder="Användarnamn"
-          required
-        />
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          placeholder="E-post"
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          placeholder="Lösenord"
-          required
-        />
-        <button type="submit">Registrera</button>
-      </form>
+      {isRegistered ? (
+        <div>
+          <h1>Välkommen, {formData.username}!</h1>
+          <p>Vill du logga in?</p>
+          <Link href="/signin">Sign In</Link>
+        </div>
+      ) : (
+        <>
+          <h1>Registrera</h1>
+          {error && <p className="error">{error}</p>}
+          <form onSubmit={handleRegister}>
+            <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              placeholder="Användarnamn"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="E-post"
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="Lösenord"
+              required
+            />
+            <button type="submit">Registrera</button>
+          </form>
+        </>
+      )}
     </div>
   );
 }
--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,3 +1,66 @@
-export default function signIn() {
-  return <div>Signin</div>;
+// src/pages/signin.tsx
+import React, { useState } from "react";
+
+export default function SignIn() {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+
+  const handleChange = (e: any) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      const response = await fetch("http://127.0.0.1:3013/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        setError(data.error || "Ett fel inträffade vid inloggning.");
+      } else {
+        // Hantera inloggad användare här, t.ex. spara token, navigera användaren etc.
+        console.log("Inloggad, token:", data.token);
+      }
+    } catch (error) {
+      setError("Kunde inte ansluta till servern.");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Logga in</h1>
+      {error && <p className="error">{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          value={credentials.username}
+          onChange={handleChange}
+          placeholder="Användarnamn"
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          onChange={handleChange}
+          placeholder="Lösenord"
+          required
+        />
+        <button type="submit">Logga in</button>
+      </form>
+    </div>
+  );
 }
